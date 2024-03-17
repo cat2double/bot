7c701eaaf1dcf9217efd85e59e377056fd925a7e
cat2double <cat2double@yandex.ru>
2024-03-17 17:23:39 +0300

    1

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const consts = require("./consts.json");
 const { WebSite } = require("./website.js");
+/*
 const schedule = require('node-schedule');
 schedule.scheduleJob(
     { minute : new schedule.Range(0, 59, 1), second: 0, tz: consts.tz },
     function () {
       console.log(new Date().toLocaleString("ru-RU", { timeZone: consts.tz }))
     })
-
+*/
 WebSite.Start()
