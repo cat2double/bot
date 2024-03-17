8853a2288e2a712d0d3e8474b83d45cccae91d09
cat2double <cat2double@yandex.ru>
2024-03-17 10:47:14 +0300

    1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const schedule = require('node-schedule');
 const consts = require("./consts.json");
+/*
+const schedule = require('node-schedule');
 schedule.scheduleJob(
     { minute : new schedule.Range(0, 59, 1), second: 0, tz: consts.tz },
     async function () {
       //await fetch("https://batl.onrender.com")
       console.log(new Date().toLocaleString("ru-RU", { timeZone: consts.tz }))
     })
+*/
+console.log(new Date().toLocaleString("ru-RU", { timeZone: consts.tz }))
