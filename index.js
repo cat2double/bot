fddbcf6a102ba10242a670055b3ab9b57057712b
cat2double <cat2double@yandex.ru>
2024-03-17 16:34:12 +0300

    1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const consts = require("./consts.json");
-/*
+const { WebSite } = require("./website.js");
 const schedule = require('node-schedule');
 schedule.scheduleJob(
     { minute : new schedule.Range(0, 59, 1), second: 0, tz: consts.tz },
@@ -7,5 +7,5 @@ schedule.scheduleJob(
       //await fetch("https://batl.onrender.com")
       console.log(new Date().toLocaleString("ru-RU", { timeZone: consts.tz }))
     })
-*/
-console.log(new Date().toLocaleString("ru-RU", { timeZone: consts.tz }))
+
+WebSite.Start()
---
+++ website.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+class WebSite {
+    constructor() {
+
+    }
+    static Start() {
+        const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>`
+      
+        const app = express();
+        const port = process.env.PORT || 3001;
+
+        app.get("/", (req, res) => res.type('html').send(html));
+
+        const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+        server.keepAliveTimeout = 120 * 1000;
+        server.headersTimeout = 120 * 1000;
+
+        
+    }
+}
+
+module.exports = {
+    WebSite
+}
